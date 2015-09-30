076bdfa89b196f9f8e2435e59d1a92ea4c02ecdc
mrandrewandrade <andrew@andrewandrade.ca>
2015-09-30 06:15:56 -0400

    fixed build to root

--- buildall.sh
+++ buildall.sh
@@ -1,3 +1,5 @@
 cd manufacturing-resume
 pdflatex andrew-andrade-manufacturing.tex
 convert -thumbnail x500 -background white andrew-andrade-manufacturing.pdf manufacturing_thumbnail.png
+mv andrew-andrade-manufacturing.pdf ../.
+mv manufacturing_thumbnail.png ../.
