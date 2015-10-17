d054153ad6da489b0a51fff3b0599f134a261424
mrandrewandrade <andrew@andrewandrade.ca>
2015-10-17 13:59:40 -0400

    fixed garbage in master branch

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,8 @@
+#Simple script to typeset resumes
+#and structure master branch
 cd manufacturing-resume
+pdflatex andrew-andrade-manufacturing.tex
+#run twice to ensure latex typesets correctly
 pdflatex andrew-andrade-manufacturing.tex
 convert -thumbnail x500 -background white andrew-andrade-manufacturing.pdf manufacturing_thumbnail.png
 mv andrew-andrade-manufacturing.pdf ../.
