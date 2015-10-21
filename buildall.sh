233043651b50a9686797821ce918ec67630a0f40
mrandrewandrade <andrew@andrewandrade.ca>
2015-10-21 01:21:53 -0400

    just putting pdfs on master branch

--- buildall.sh
+++
@@ -1,9 +0,0 @@
-#Simple script to typeset resumes
-#and structure master branch
-cd manufacturing-resume
-pdflatex andrew-andrade-manufacturing.tex
-#run twice to ensure latex typesets correctly
-pdflatex andrew-andrade-manufacturing.tex
-convert -thumbnail x500 -background white andrew-andrade-manufacturing.pdf manufacturing_thumbnail.png
-mv andrew-andrade-manufacturing.pdf ../.
-mv manufacturing_thumbnail.png ../.
